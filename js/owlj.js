8e9566606465c315be91c844c607d57471aaac22
tony-medici <tonyudeagbala@gmail.com>
2019-12-15 00:48:47 +0100

    My first commit.

---
+++ js/owlj.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("#owl-demo").owlCarousel({
+		autoPlay: 9000, //Set AutoPlay to 1 seconds
+		pagination:false,
+		items : 4,
+		itemsDesktop : [1199.3],
+		itemsDesktopSmall :[979.3]
+	});
+});
